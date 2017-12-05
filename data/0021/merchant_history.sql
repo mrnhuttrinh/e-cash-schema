0e3fad4e72873896af371ac2995543743b81b7c8
Trinh Nguyen <nntrinh@tma.com.vn>
2017-12-05 22:09:24 +0700

    add merchant history data

---
+++ data/0021/merchant_history.sql
@@ -0,0 +1,169 @@
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  '9f519f35-6d68-428b-ad18-3a182e6c7584',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-222222222222',
+  'CREATED',
+  '{"previous":{},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-11 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  'e8c2c774-e21a-4948-89f9-2a3f9b99902f',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-222222222222',
+  'ADDED',
+  '{"previous":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-12 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  '9f9a9ca5-2061-4f62-9f43-555c332f124c',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-222222222222',
+  'UPDATED',
+  '{"previous":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "INACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-13 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  'e739bcd6-828e-41f8-913e-ed25e96e201f',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-222222222222',
+  'UPDATED',
+  '{"previous":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "INACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-222222222222", name: "Test merchant 02", "phone": "+84283247248", "email": "test_merchant02@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-222222222222", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-14 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  '68ff136f-c0d3-4952-8128-60d00ee19b3c',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-111111111111',
+  'CREATED',
+  '{"previous":{},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-11 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  '6869bf6b-9828-4488-9ca9-241d18743f6a',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-111111111111',
+  'ADDED',
+  '{"previous":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-12 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  '37170ecd-7ce4-497f-b571-cfb6a949205f',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-111111111111',
+  'UPDATED',
+  '{"previous":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "INACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-13 01:18:27',
+  NULL
+);
+
+INSERT INTO merchant_history
+(
+  id,
+  created_by,
+  merchant_id,
+  history_type,
+  history_details,
+  created_at,
+  updated_at
+)
+VALUES
+(
+  '71437bb4-efd6-49d9-a1c3-cc2e22e7e659',
+  '6dad4cb7438d4a308ce49bfb04157245',
+  '0f57bb0a-3cd6-47ec-ad95-111111111111',
+  'UPDATED',
+  '{"previous":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "INACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null},"next":{"id": "0f57bb0a-3cd6-47ec-ad95-111111111111", name: "Smart World", "phone": "+84283247247", "email": "test_merchant01@yopmail.com", "address_id": "48c3a336-dc25-49bc-bae1-111111111111", "status": "ACTIVE", created_at: "2017-11-11 15:45:33", updated_at: null}}',
+  '2017-11-14 01:18:27',
+  NULL
+);
+
+COMMIT;
